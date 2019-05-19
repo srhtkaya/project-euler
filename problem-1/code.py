10cb2d5bc7d0d4e0027cc5459ef3f9b52a3d914f
Serhat Kaya <kaya.serrhat@gmail.com>
2019-05-19 11:30:47 +0300

    Added solution to problem 1 with functional programming

--- problem-1/code.py
+++ problem-1/code.py
@@ -1,8 +1,17 @@
+from rx import from_list, operators as op
 max_number = 1000
+
+# Legacy method
 multiples = []
 
 for number in range(max_number):
-    if(number%3 == 0 or number%5 == 0):
+    if(number % 3 == 0 or number % 5 == 0):
         multiples.append(number)
 
 print(sum(multiples))
+
+# With reactive programming
+from_list(range(max_number)).pipe(
+    op.filter(lambda n: n % 3 == 0 or n % 5 == 0),
+    op.sum()
+).subscribe(lambda s: print(s))
