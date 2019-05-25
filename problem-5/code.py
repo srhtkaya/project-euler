94c2d579843cbaa1eb4def66ae97091a84049844
Serhat Kaya <kaya.serrhat@gmail.com>
2019-05-25 10:00:35 +0300

    Solution of problem 5

---
+++ problem-5/code.py
@@ -0,0 +1,20 @@
+def gcd(number1, number2):
+    if number1 == 0:
+        return number2
+    else:
+        return gcd(number2 % number1, number1)
+
+
+def lcm(number1, number2):
+    return int(number1 * number2 / gcd(number1, number2))
+
+
+def find_product(max_number):
+    product = 1
+    for number in range(2, max_number+1):
+        product = lcm(product, number)
+
+    return product
+
+
+print(find_product(20))
